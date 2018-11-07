13d63f6bbd24fb595f8905d5219950ed34263b5b
atkinsonholly <atkinsonholly@gmail.com>
2018-11-07 08:49:54 +0000

    Lab completed

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+	if speed < 40 || speed > 60
+		true
+	else
+		false
+	end
 end
-
+#takes an argument of a speed and return true if unsafe
+#and false if it is safe
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
-	
-
-
+#same outcome using ternary operator
